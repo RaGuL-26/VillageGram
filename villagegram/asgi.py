f73c421d7da0eab501b7106743a9b4c8d43f2e08
ragul <ragulraja893@gmail.com>
2024-05-24 18:12:51 +0530

    second-commit

--- villagegram/asgi.py
+++ villagegram/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'villagegram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'villagegram.depo')
 
 application = get_asgi_application()
